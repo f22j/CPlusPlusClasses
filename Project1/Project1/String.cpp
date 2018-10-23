446e67a6598f12cacf66e88b6fed2838ff614d98
knucklesuganda <35567046+knucklesuganda@users.noreply.github.com>
2018-10-23 22:12:11 +0600

    String erase method and += operator

--- Project1/Project1/String.cpp
+++ Project1/Project1/String.cpp
@@ -1,8 +1,4 @@
-#pragma once
 #include "String.h"
-
-#include <iostream>
-using namespace std;
 
 String::String() {
 
@@ -11,14 +7,14 @@ String::String() {
 
 }
 
-String::String(int len){
-	
+String::String(int len) {
+
 	length = len;
 	str = new char[len];
 
 }
 
-String::String(const char *str){
+String::String(const char *str) {
 
 	this->length = strlen(str);
 	this->str = new char[length + 1];
@@ -26,7 +22,7 @@ String::String(const char *str){
 
 }
 
-String::String(const String &copy_str){
+String::String(const String &copy_str) {
 
 	this->length = copy_str.length;
 	this->str = new char[this->length];
@@ -34,17 +30,21 @@ String::String(const String &copy_str){
 
 }
 
-int String::get_length(){
+int String::get_length() const {
 	return length;
 }
 
-const char *String::get_string(){
+const char *String::get_string() const {
 	return str;
+}
+
+const char String::get_symbol(int pos) const {
+	return this->str[pos];
 }
 
 void String::set_str(const char * str) {
 
-	if (length != strlen(str)){
+	if (length != strlen(str)) {
 
 		delete[] this->str;
 		this->length = strlen(str);
@@ -56,11 +56,11 @@ void String::set_str(const char * str) {
 
 }
 
-void String::print(){
+void String::print() const {
 	cout << str;
 }
 
-void String::input(){
+void String::input() {
 
 	char cpstr[1024];
 	cin.getline(cpstr, 1024);
@@ -74,16 +74,14 @@ void String::clear() {
 
 }
 
-void String::insert(int pos, const char *str){
+void String::insert(int pos, const char *str) {
 
-	if (pos > length || pos < 0 || str == nullptr) 
+	if (pos > length || pos < 0 || str == nullptr)
 		return;
 
-	if(length == 0 && pos == 0){
-
+	else if (length == 0 && pos == 0) {
 		set_str(str);
 		return;
-
 	}
 
 	char *temp_str = new char[length + strlen(str) + 1];
@@ -92,10 +90,10 @@ void String::insert(int pos, const char *str){
 		temp_str[i] = this->str[i];
 
 	for (int i = pos; i < strlen(str) + pos; i++)
-		temp_str[i] = str[i - pos];
+		temp_str[i] = str[i - strlen(str)];
 
 	for (int i = pos + strlen(str); i < length + strlen(str); i++)
-		temp_str[i] = this->str[i - strlen(str)];
+		temp_str[i] = this->str[i - pos];
 
 	temp_str[length + strlen(str) - 1] = '\0';
 
@@ -105,18 +103,46 @@ void String::insert(int pos, const char *str){
 
 }
 
-void String::append(const char * str){
+void String::append(const char * str) {
 	this->insert(length, str);
 }
 
-void String::erase(int pos, int length){
+void String::erase(int pos, int del_len) {
+
+	if (pos > this->length || pos < 0 || str == nullptr || del_len == 0)
+		return;
+
+	char *copy_str = new char[(this->length - del_len) + 1];
+
+	for (int i = 0; i < pos; i++)
+		copy_str[i] = str[i];
+
+	for (int i = del_len + pos; i <= this->length; i++)
+		copy_str[i + 1 - pos - del_len] = str[i];
+
+	set_str(copy_str);
+
 }
 
-String::~String(){
+String & String::operator+=(const String & str) {
 
-	if(length != 0)delete[] str;
+	this->append(str.str - 1);
+	return *this;
+
+}
+
+String::~String() {
+
+	if (length != 0)delete[] str;
 
 	str = nullptr;
 	length = 0;
 
+}
+
+
+ostream & operator<<(ostream & os, const String &str) {
+
+	for (int i = 0; i < str.get_length(); i++)cout << str.get_symbol(i);
+	return os;
 }
--- Project1/Project1/String.h
+++ Project1/Project1/String.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class String{
 
 	char *str;
@@ -10,19 +13,23 @@ public:
 	String(const char *str);
 	String(const String &copy_str);
 
-	int get_length();
-	const char *get_string();
+	int get_length() const;
+	const char get_symbol(int pos) const;
+	const char *get_string() const;
+
 	void set_str(const char *str);
 
-	void print();
+	void print() const;
 	void input();
 	void clear();
 
 	void insert(int pos, const char *str);
 	void append(const char *str);
-
 	void erase(int pos, int length);
 
-	~String();
+	String & operator+=(const String & str);
 
+	~String();
 };
+
+ostream & operator<<(ostream &os, const String &str);
