3d8563677d70309c523ec30adf1815eadca936d4
knucklesuganda <35567046+knucklesuganda@users.noreply.github.com>
2018-10-29 20:26:48 +0600

    sad

--- itstephomeworks_project/itstephomeworks_project/main.cpp
+++ itstephomeworks_project/itstephomeworks_project/main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
 int main() {
-
-	const char str[] = "-------------------LOADING-------------------";
-
-	while (true) {
-
-		for (char symb : str) {
-			Sleep(100);
-			cout << symb;
-		}
-
-		Sleep(1000);
-
-		system("cls");
-
-	}
-
+	system("pause");
 }
