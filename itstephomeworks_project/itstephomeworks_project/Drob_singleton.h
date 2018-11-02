44a7cdd5a085ef7581629f593928316f2449289d
knucklesuganda <35567046+knucklesuganda@users.noreply.github.com>
2018-11-02 10:05:57 +0600

    Really final super drob singleton

--- itstephomeworks_project/itstephomeworks_project/Drob_singleton.h
+++ itstephomeworks_project/itstephomeworks_project/Drob_singleton.h
@@ -4,7 +4,9 @@ using namespace std;
 
 class Drob_singleton {
 
-	Drob_singleton();
+	int x, y;
+
+	Drob_singleton(int x, int y);
 	Drob_singleton(const Drob_singleton&) = delete;
 	Drob_singleton operator=(const Drob_singleton&) = delete;
 
@@ -12,14 +14,30 @@ class Drob_singleton {
 
 public:
 
-	static Drob_singleton* getDrob_singleton() {
+	static Drob_singleton* getDrob_singleton(int x, int y) {
 
 		if (instance == nullptr)
-			instance = new Drob_singleton();
+			instance = new Drob_singleton(x, y);
 
 		return instance;
 
 	}
+
+	Drob_singleton operator=(int drob_value);
+
+	int get_x() const;
+	int get_y() const;
+
+	void set_x(int x);
+	void set_y(int y);
+
+	void print() const;
+
+	operator double() const;
+	operator float() const;
+	operator int() const;
+
+	float get_drob()const;
 
 	~Drob_singleton() {
 		delete instance;
