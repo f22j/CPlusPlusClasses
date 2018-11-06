0b393bac37379267f3210dba5c0114c0331a70d4
knucklesuganda <35567046+knucklesuganda@users.noreply.github.com>
2018-11-06 20:32:38 +0600

    фa

--- itstephomeworks_project/itstephomeworks_project/main.cpp
+++ itstephomeworks_project/itstephomeworks_project/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include "Date.h"
 #include <string>
 using namespace std;
 
 int	main(){
 
-	
+	int n, k;
+	cin >> n >> k;
+
+	int size_n;
+
+	while (n > 0) {
+
+		n / 10;
+		size_n += 1;
+
+	}
+
+	string str;
+
 	system("pause");
 }
